2129de0ee9fd83c3f410541caf3b5b14c897ea70
GaspardMzt <gaspard.mzt@gmail.com>
2022-05-25 17:14:40 +0200

    init the prisma DBB

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import HeaderStyle from "../styles/Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <nav>
-        <Link href="/" className={HeaderStyle.link}>
+        <Link href="/">
           <a>Home</a>
         </Link>
         <Link href="/articles">

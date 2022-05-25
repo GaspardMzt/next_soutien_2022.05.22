7192f45f2a89baa7e6a841ccb3090d6321ea30c5
GaspardMzt <gaspard.mzt@gmail.com>
2022-05-25 16:56:11 +0200

    prisma init

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import HeaderStyle from "../styles/Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/articles">Articles</Link>
+        <Link href="/" className={HeaderStyle.link}>
+          <a>Home</a>
+        </Link>
+        <Link href="/articles">
+          <a>Articles</a>
+        </Link>
       </nav>
     </header>
   );
